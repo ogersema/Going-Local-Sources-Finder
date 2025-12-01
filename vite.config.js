3c8503df8a389073c29bff506a5d32e4bad6e219
Olaf Gersemann <olaf.gersemann@welt.de>
2025-12-01 12:10:01 +0100

    Fix vite config syntax

--- vite.config.js
+++ vite.config.js
@@ -14,3 +14,14 @@ git add .
 git commit -m "Fix base URL case"
 git push
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Going-Local-Sources-Finder/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
+})
