f96dc68cd14efa28ef62e1e05fec149310dfb6c9
Olaf Gersemann <olaf.gersemann@welt.de>
2025-12-01 11:29:44 +0100

    Fix base URL case

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/going-local-sources-finder/', // GitHub repo name - ANPASSEN falls anders!
+  base: '/Going-Local-Sources-Finder/', // GitHub repo name - ANPASSEN falls anders!
   build: {
     outDir: 'dist',
     sourcemap: false
   }
 })
+git add .
+git commit -m "Fix base URL case"
+git push
+
