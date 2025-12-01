f9b872ee4c45c0e043df1076ea975e147f7f8845
OlafG <63862688+ogersema@users.noreply.github.com>
2025-12-01 11:14:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lokaler-quellen-finder/', // GitHub repo name - ANPASSEN falls anders!
+  base: '/going-local-sources-finder/', // GitHub repo name - ANPASSEN falls anders!
   build: {
     outDir: 'dist',
     sourcemap: false
