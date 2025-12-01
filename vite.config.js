190411e8f97f622baf48778d8604ee107f72f2f6
Olaf Gersemann <olaf.gersemann@welt.de>
2025-12-01 12:29:00 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/going-local-sources-finder/', // GitHub repo name - ANPASSEN falls anders!
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
-})
-git add .
-git commit -m "Fix base URL case"
-git push
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   base: '/Going-Local-Sources-Finder/',
